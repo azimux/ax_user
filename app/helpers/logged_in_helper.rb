27b5df8c6e56ac001264d59c107d242bb69ffc19
Miles Georgi <azimux@gmail.com>
2010-03-03 11:53:31 -0800

    added a logged_in? helper for views

--- app/helpers/logged_in_helper.rb
+++ app/helpers/logged_in_helper.rb
@@ -1,7 +1,15 @@
 module LoggedInHelper
   def logged_in_as_admin?
-    if @ax_user
-      @ax_user.in_role?('admin')
+    if user
+      user.in_role?('admin')
     end
+  end
+
+  def logged_in?
+    user
+  end
+
+  def user
+    @ax_user
   end
 end
