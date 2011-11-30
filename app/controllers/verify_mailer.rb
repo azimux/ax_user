16fcb48d912979f251384e529253343eeaefe2fa
Miles Georgi <azimux@gmail.com>
2011-11-30 12:44:33 -0800

    passing a hash via @body is now deprecated in ActionMailer

--- app/controllers/verify_mailer.rb
+++ app/controllers/verify_mailer.rb
@@ -1,7 +1,7 @@
 class VerifyMailer < ActionMailer::Base
   def verify(user)
     @subject    = "Thank you for registering with #{$site_name}"
-    @body       = {:user => user}
+    @user       = user
     @recipients = user.email
     @from       = $from_email_string
     @sent_on    = Time.now
@@ -9,7 +9,7 @@ class VerifyMailer < ActionMailer::Base
 
   def forgot_password(user)
     @subject    = "#{$site_name} password request"
-    @body       = {:user => user}
+    @user       = user
     @recipients = user.email
     @from       = $from_email_string
     @sent_on    = Time.now
